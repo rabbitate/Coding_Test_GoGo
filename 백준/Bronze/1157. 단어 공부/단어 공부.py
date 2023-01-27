98fe9c688d19c3ef424c471a2f77600638db296e
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2023-01-27 16:12:46 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 32408 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,18 +1,9 @@
-alphabet = [0 for _ in range(26)]
+s = input().upper()
+alist = list(set(s))
+cntList = []
 
-s = input()
-s = s.upper()
+for c in alist:
+    cntList.append(s.count(c))
 
-for c in s:
-    alphabet[ord(c) - ord('A')] += 1
-    
-maxcnt = max(alphabet)
-cnt = 0
-
-for index in range(len(alphabet)):
-    if alphabet[index] == maxcnt:
-        cnt += 1
-        maxalpha = chr(ord('A') + index)
-
-if cnt == 1: print(maxalpha)
-else: print("?")
+if cntList.count(max(cntList)) >= 2: print("?")
+else: print(alist[cntList.index(max(cntList))])
