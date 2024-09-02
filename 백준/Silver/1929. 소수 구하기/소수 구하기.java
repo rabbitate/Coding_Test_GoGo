27bc2b401c37e5ea13b2ec4891d53bdfbf15cdbd
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2024-09-02 23:28:25 +0900

    [Silver III] Title: 소수 구하기, Time: 848 ms, Memory: 33364 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        boolean[] isPrime = new boolean[n+1];
+
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!isPrime[i]) continue;
+            for (int j = 2; i*j <= n; j++) {
+                isPrime[i*j] = false;
+            }
+        }
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+}
