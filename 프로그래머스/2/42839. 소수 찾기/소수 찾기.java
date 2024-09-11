6305259407d18796b8dbdd90cb8a7c979eea2681
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2024-09-12 02:42:36 +0900

    [level 2] Title: 소수 찾기, Time: 30.10 ms, Memory: 91.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,46 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    static boolean[] visited = new boolean[7];
+    static Set<Integer> set;
+    public int solution(String numbers) {
+        int answer = 0;
+         set = new HashSet<>();
+        
+        dfs(numbers, "", 0);
+        
+        for (int i : set) {
+            if(isPrime(i)) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    void dfs(String numbers, String number, int depth) {
+        if (depth == numbers.length()) {
+            return;
+        }
+        
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                set.add(Integer.parseInt(number + numbers.charAt(i)));
+                dfs(numbers, number+numbers.charAt(i), depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+    
+    boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
