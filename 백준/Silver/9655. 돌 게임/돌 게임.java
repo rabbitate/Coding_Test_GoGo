f4012723a66fbe710bd930eef638d24b6de73688
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2024-12-09 16:41:04 +0900

    [Silver V] Title: 돌 게임, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,7 +6,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if(n % 2 == 1) {
+        int[] dp = new int[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 1;
+
+        for (int i = 4; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+        }
+
+        if (dp[n] % 2 == 1) {
             System.out.println("SK");
         } else {
             System.out.println("CY");
