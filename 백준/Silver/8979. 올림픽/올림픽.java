954b9f5e6d2583fda244520e241b4db4e988cac8
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2024-12-10 16:50:02 +0900

    [Silver V] Title: 올림픽, Time: 128 ms, Memory: 14652 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -11,39 +11,49 @@ public class Main {
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int k = Integer.parseInt(stringTokenizer.nextToken());
 
-        int[][] country = new int[n+1][3];
+        int[][] country = new int[n][4];
 
         for (int i = 0; i < n; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int c = Integer.parseInt(stringTokenizer.nextToken());
-            for (int j = 0; j < 3; j++) {
-                country[c][j] = Integer.parseInt(stringTokenizer.nextToken());
+            for (int j = 0; j < 4; j++) {
+                country[i][j] = Integer.parseInt(stringTokenizer.nextToken());
             }
         }
 
         Arrays.sort(country, (a, b) -> {
-            if (a[0] != b[0]) {
-                return a[0] - b[0];
+            if (a[1] != b[1]) {
+                return b[1] - a[1];
             } else {
-                if (a[1] != b[1]) {
-                    return a[1] - b[1];
+                if (a[2] != b[2]) {
+                    return b[2] - a[2];
                 } else {
-                    return a[2] - b[2];
+                    return b[3] - a[3];
                 }
             }
         });
 
         int rank = 1;
-        int stdSum = Arrays.stream(country[0]).sum();
+        int cnt = 1;
 
         for (int i = 0; i < n; i++) {
-            int sum = Arrays.stream(country[i]).sum();
-            if (sum != stdSum) {
-                rank++;
-                stdSum = sum;
+            if (i > 0) {
+                // 이전 국가와 메달 비교
+                if (country[i][1] == country[i - 1][1] &&
+                        country[i][2] == country[i - 1][2] &&
+                        country[i][3] == country[i - 1][3]) {
+                    // 메달 수가 같으면 순위 유지
+                    cnt++;
+                } else {
+                    // 메달 수가 다르면 순위 갱신
+                    rank += cnt;
+                    cnt = 1;
+                }
             }
-            if (i == k) {
+
+            // 찾고자 하는 국가의 번호일 경우 출력 후 종료
+            if (country[i][0] == k) {
                 System.out.println(rank);
+                break;
             }
         }
     }
