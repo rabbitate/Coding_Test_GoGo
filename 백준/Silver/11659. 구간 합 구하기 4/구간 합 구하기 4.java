fb42d8101274ac4a633501c60ef344ca9f996b2a
Minpyo Kim <56537513+rabbitate@users.noreply.github.com>
2025-02-15 15:39:27 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 704 ms, Memory: 64244 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -7,37 +7,38 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
 
         int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
-        int[] numArray = new int[n];
-        int[] sumArray = new int[n];
+        int caseNum = Integer.parseInt(stringTokenizer.nextToken());
+
+        int[] num = new int[n];
+        int[] dp = new int[n+1];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
 
         for (int i = 0; i < n; i++) {
-            numArray[i] = Integer.parseInt(stringTokenizer.nextToken());
-            if (i == 0) {
-                sumArray[i] = numArray[i];
-            } else {
-                sumArray[i] = sumArray[i-1] + numArray[i];
-            }
+            num[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
 
-        for (int k = 0; k < m; k++) {
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i-1] + num[i-1];
+        }
+
+        for (int i = 0; i < caseNum; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int i = Integer.parseInt(stringTokenizer.nextToken())-1;
-            int j = Integer.parseInt(stringTokenizer.nextToken())-1;
 
-            int answer;
-            if (i == 0) {
-                answer = sumArray[j];
-            } else {
-                answer = sumArray[j] - sumArray[i-1];
+            int start = Integer.parseInt(stringTokenizer.nextToken());
+            int end = Integer.parseInt(stringTokenizer.nextToken());
+
+            if (start == end) {
+                stringBuilder.append(num[start - 1]).append('\n');
+                continue;
             }
 
-            System.out.println(answer);
+            stringBuilder.append(dp[end]-dp[start-1]).append('\n');
         }
 
+        System.out.println(stringBuilder.toString());
     }
 }
